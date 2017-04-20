6744dac315a06a9a015a9342fa55ca5e7ca36e84
hubcarl <hubcarl@126.com>
2017-04-20 11:52:58 +0800

    refactor: 修改vueEgnine 为 vue

--- app/extend/application.js
+++ app/extend/application.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const Engine = require('../../lib/engine');
-const VUEENGINE = Symbol('Application#VueEngine');
+const VUE_ENGINE = Symbol('Application#vue');
 
 module.exports = {
 
-  get vueEngine() {
-    if (!this[VUEENGINE]) {
-      this[VUEENGINE] = new Engine(this);
+  get vue() {
+    if (!this[VUE_ENGINE]) {
+      this[VUE_ENGINE] = new Engine(this);
     }
-    return this[VUEENGINE];
+    return this[VUE_ENGINE];
   },
 };
